639f1fac0be14b6558d7a815b04d29df48049a8a
blackBuzo2601 <elianpro58@gmail.com>
2024-12-12 16:27:23 -0800

    Reestructurado condicional inicial, para hacer suma del segundo par

--- Problema-1/app.js
+++ Problema-1/app.js
@@ -3,14 +3,18 @@
 un número ingresado por el usuario. La mecánica de la carrera dependerá de ciertas 
 condiciones.*/
 
-const numeroIntroducido = 2342554354; //numero introducido por el usuario
+const numeroIntroducido = 2342554354; //numero introducido por el usuario //10 caracteres
 const carroX = 0;
 const carroY = 0;
 const numeroIntroducidoString = numeroIntroducido.toString();
 let contador = -1;
+let letras = 0;
 let valorUno = 0;
 let valorDos = 0;
-let sumaPrimerPar = 0;
+let segundoValorUno = 0;
+let segundoValorDos = 0;
+
+let sumaSegundoPar = 0;
 let numeroUno = 0;
 if (numeroIntroducido < 0) {
   console.log("No introduzcas numeros negativos");
@@ -19,15 +23,20 @@ if (numeroIntroducido < 0) {
   for (let i = 0; i < numeroIntroducidoString.length; i++) {
     //iterar por la cantidad de digitos
     contador++; //0
-
-    valorUno = numeroIntroducidoString[contador]; //tomaar el primer numero
-    contador++; //1
-
-    valorDos = numeroIntroducidoString[contador];
-
-    let suma = new String(valorUno + "+" + valorDos);
-    console.log(eval(suma.valueOf())); //con valueOf podemos convertir un objeto string a su
-    //contraparte definitiva. es lo que queremos, queremos convertir un string a numero
-    //(debido a que no existe algo como to.Number() al igual que toString())
+    if (contador != 1) {
+      segundoValorUno = numeroIntroducidoString[contador];
+      segundoValorDos = numeroIntroducidoString[contador];
+      let sumaSegundoPar = new String(segundoValorUno + "+" + segundoValorDos);
+      console.log("Imprimiendo suma del segundo par: " + sumaSegundoPar);
+      //cuando contador vale 1, es porque ya se iteró dos veces
+    } else {
+      valorUno = numeroIntroducidoString[contador]; //tomaar el primer numero
+      contador++; //1
+      valorDos = numeroIntroducidoString[contador];
+      let sumaPrimerPar = new String(valorUno + "+" + valorDos);
+      console.log(eval(sumaPrimerPar.valueOf())); //con valueOf podemos convertir un objeto string a su
+      //contraparte definitiva. es lo que queremos, queremos convertir un string a numero
+      //(debido a que no existe algo como to.Number() al igual que toString())
+    }
   }
 }
